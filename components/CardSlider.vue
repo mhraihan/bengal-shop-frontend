5feea8ca00534cab10e5b92e07b3cd9a95e4b21f
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2022-02-02 05:53:20 +0600

    brand slider

---
+++ components/CardSlider.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container pt-20">
+    <div class="flex items-center justify-between card-title">
+      <h3 class="mb-6 text-4xl text-gray-300">
+        {{ data.title }}
+      </h3>
+      <div class="card-arrows min-w-max">
+        <button
+          @click="prev"
+          class="w-8 h-8 p-1 mr-4 transition-all rounded-full  arrow-left hover:bg-green-500 hover:text-white"
+        >
+          <font-awesome-icon class="" :icon="['fas', 'chevron-left']" />
+        </button>
+        <button
+          @click="next"
+          class="w-8 h-8 p-1 transition-all rounded-full  arrow-right hover:bg-green-500 hover:text-white"
+        >
+          <font-awesome-icon class="" :icon="['fas', 'chevron-right']" />
+        </button>
+      </div>
+    </div>
+    <div class="flex items-center mb-10">
+      <div style="height: 3px" class="bg-green-500 w-32 h-0.5"></div>
+      <div class="bg-gray-800 w-full h-0.5"></div>
+    </div>
+    <vue-slick-carousel
+      class="mb-10 text-center card-carousel"
+      v-bind="data.settings"
+      ref="cardCarousel"
+    >
+      <div
+        v-for="(card, index) in data.data"
+        :key="index"
+        class="flex flex-col items-center justify-center p-4 text-gray-300 cursor-pointer  card-carousel-item"
+        :class="data.class"
+      >
+        <div class="flex flex-col items-center justify-center h-24 mb-4">
+          <img class="h-full" :src="src(card.img)" alt="" />
+        </div>
+        <h3 v-if="card.title" class="mb-3 text-xl">{{ card.title }}</h3>
+      </div>
+    </vue-slick-carousel>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    src(img) {
+      return require(`~/assets/img/${img}`);
+    },
+    prev() {
+      this.$refs.cardCarousel.prev();
+    },
+    next() {
+      this.$refs.cardCarousel.next();
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,9 @@
   <div class="min-h-full">
     <div class="p-8">
       <Slideshow />
-      <CategorySlider />
+      <CardSlider :data="categories" />
       <PromoBanner />
+      <CardSlider :data="brands" />
     </div>
   </div>
 </template>
@@ -11,5 +12,109 @@
 <script>
 export default {
   name: "IndexPage",
+  data() {
+    return {
+      categories: {
+        title: "Search by Category",
+        class: "bg-orange-700 hover:bg-green-100",
+        settings: {
+          slidesToShow: 6,
+          dots: false,
+          arrows: false,
+        },
+        data: [
+          {
+            img: "carousel-img-1.png",
+            title: "Groceries",
+            url: "/",
+          },
+          {
+            img: "carousel-img-2.png",
+            title: "Fish",
+            url: "/",
+          },
+          {
+            img: "carousel-img-3.png",
+            title: "Meat",
+            url: "/",
+          },
+          {
+            img: "carousel-img-4.png",
+            title: "Vegetables",
+            url: "/",
+          },
+          {
+            img: "carousel-img-5.png",
+            title: "Fruits",
+            url: "/",
+          },
+          {
+            img: "carousel-img-6.png",
+            title: "Ice Cream",
+            url: "/",
+          },
+          {
+            img: "carousel-img-4.png",
+            title: "Juice",
+            url: "/",
+          },
+          {
+            img: "carousel-img-3.png",
+            title: "Chocolate",
+            url: "/",
+          },
+        ],
+      },
+      brands: {
+        title: "Popular Brands",
+        settings: {
+          slidesToShow: 6,
+          dots: false,
+          arrows: false,
+        },
+        data: [
+          {
+            img: "brand-1.png",
+            url: "/",
+          },
+          {
+            img: "brand-2.png",
+
+            url: "/",
+          },
+          {
+            img: "brand-3.png",
+
+            url: "/",
+          },
+          {
+            img: "brand-4.png",
+
+            url: "/",
+          },
+          {
+            img: "brand-5.png",
+
+            url: "/",
+          },
+          {
+            img: "brand-6.png",
+
+            url: "/",
+          },
+          {
+            img: "brand-4.png",
+
+            url: "/",
+          },
+          {
+            img: "brand-3.png",
+
+            url: "/",
+          },
+        ],
+      },
+    };
+  },
 };
 </script>
