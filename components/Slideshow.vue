0f5285066c25fba8b07202cdf4f3c80b332a0583
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2022-02-02 06:02:57 +0600

    added font size and line height

--- components/Slideshow.vue
+++ components/Slideshow.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="hero-slide">
     <vue-slick-carousel :arrows="false" :dots="true">
-      <div class="bg-green-100 rounded-2xl pt-8">
+      <div class="pt-8 bg-green-100 rounded-2xl">
         <div class="container">
           <div class="flex items-center">
-            <div class="mr-6 w-1/2 font-size-22">
-              <p class="text-green-300 font-size-32 mb-4">Save up 30% off</p>
-              <h2 class="text-7xl font-bold text-gray-300 mb-6">
+            <div class="w-1/2 mr-6 font-size-22">
+              <p class="mb-4 text-green-300 font-size-32">Save up 30% off</p>
+              <h2 class="mb-6 font-bold text-gray-300 font-size-70">
                 Bengal Vegetable farm Organic 100%
               </h2>
               <p class="mb-8">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Vehicula faucibus massa est elit maecenas.
               </p>
-              <nuxt-link to="/" class="bs-button text-base">Shop Now</nuxt-link>
+              <nuxt-link to="/" class="text-base bs-button">Shop Now</nuxt-link>
             </div>
             <div class="flex justify-end w-1/2">
               <img
@@ -27,19 +27,19 @@
           </div>
         </div>
       </div>
-      <div class="bg-green-100 rounded-2xl pt-8">
+      <div class="pt-8 bg-green-100 rounded-2xl">
         <div class="container">
           <div class="flex items-center">
-            <div class="mr-6 w-1/2 font-size-22">
-              <p class="text-green-300 font-size-32 mb-4">Save up 40% off</p>
-              <h2 class="text-7xl font-bold text-gray-300 mb-6">
+            <div class="w-1/2 mr-6 font-size-22">
+              <p class="mb-4 text-green-300 font-size-32">Save up 40% off</p>
+              <h2 class="mb-6 font-bold text-gray-300 text-7xl">
                 Bengal Vegetable farm Organic 100%
               </h2>
               <p class="mb-8">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Vehicula faucibus massa est elit maecenas.
               </p>
-              <nuxt-link to="/" class="bs-button text-base">Shop Now</nuxt-link>
+              <nuxt-link to="/" class="text-base bs-button">Shop Now</nuxt-link>
             </div>
             <div class="flex justify-end w-1/2">
               <img
@@ -53,19 +53,19 @@
           </div>
         </div>
       </div>
-      <div class="bg-green-100 rounded-2xl pt-8">
+      <div class="pt-8 bg-green-100 rounded-2xl">
         <div class="container">
           <div class="flex items-center">
-            <div class="mr-6 w-1/2 font-size-22">
-              <p class="text-green-300 font-size-32 mb-4">Save up 50% off</p>
-              <h2 class="text-7xl font-bold text-gray-300 mb-6">
+            <div class="w-1/2 mr-6 font-size-22">
+              <p class="mb-4 text-green-300 font-size-32">Save up 50% off</p>
+              <h2 class="mb-6 font-bold text-gray-300 text-7xl">
                 Bengal Vegetable farm Organic 100%
               </h2>
               <p class="mb-8">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Vehicula faucibus massa est elit maecenas.
               </p>
-              <nuxt-link to="/" class="bs-button text-base">Shop Now</nuxt-link>
+              <nuxt-link to="/" class="text-base bs-button">Shop Now</nuxt-link>
             </div>
             <div class="flex justify-end w-1/2">
               <img
