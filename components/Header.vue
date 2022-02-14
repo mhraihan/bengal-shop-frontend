ddcdf87f92354f4c859be4de96a63c670d278cbd
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2022-02-14 08:11:58 +0600

    Header Modify

--- components/Header.vue
+++ components/Header.vue
@@ -1,82 +1,89 @@
 <template>
   <div>
     <div class="container">
-      <div class="flex flex-wrap items-center justify-between py-6">
-        <Logo />
-        <div class="w-96">
-          <div
-            class="flex items-center px-1 py-1 border border-gray-100 rounded-full "
-          >
-            <div class="px-3 miw-w-max">
-              <img
-                src="~/assets/img/search.png"
-                loading="lazy"
-                alt="search icon"
-                srcset=""
-              />
-            </div>
-            <input
-              type="search"
-              placeholder="Search here..."
-              class="w-full px-3 py-1 focus:outline-none"
-            />
-            <button
-              type="submit"
-              class="px-6 py-2 text-white bg-gray-400 rounded-full bs-button-bg"
-            >
-              Search
-            </button>
-          </div>
+      <div class="flex items-center justify-between py-6">
+        <div class="w-1/4">
+          <Logo />
         </div>
-        <div class="flex items-center min-w-max">
-          <nuxt-link to="/">
-            <span
-              class="flex items-center justify-center rounded-full  bs-icon-box hover:bg-gray-200"
-            >
-              <img
-                src="~/assets/img/heart.png"
-                loading="lazy"
-                alt="heart icon"
-                srcset=""
-              />
-            </span>
-          </nuxt-link>
-          <nuxt-link to="/" class="relative mx-4">
-            <span
-              class="flex items-center justify-center rounded-full  bs-icon-box hover:bg-gray-200"
-            >
-              <img
-                src="~/assets/img/cart.png"
-                loading="lazy"
-                alt="cart icon"
-                srcset=""
-              />
-            </span>
-            <span
-              class="absolute top-0 right-0 flex items-center justify-center w-4 h-4 text-xs text-white bg-orange-600 rounded-full "
-              >0</span
-            >
-          </nuxt-link>
-          <nuxt-link to="/" class="flex items-center">
-            <span
-              class="flex items-center justify-center rounded-full  bs-icon-box hover:bg-gray-200"
-            >
-              <img
-                src="~/assets/img/account.png"
-                loading="lazy"
-                alt="account icon"
-                srcset=""
-              />
-            </span>
-            <span>Account</span>
-          </nuxt-link>
+
+        <div class="w-3/4 pl-20">
+          <div class="flex items-center justify-between">
+            <div class="flex w-full pl-10">
+              <div
+                class="flex items-center justify-center w-3/4 px-1 py-1 border border-gray-100 rounded-full "
+              >
+                <div class="px-3 miw-w-max">
+                  <img
+                    src="~/assets/img/search.png"
+                    loading="lazy"
+                    alt="search icon"
+                    srcset=""
+                  />
+                </div>
+                <input
+                  type="search"
+                  placeholder="Search here..."
+                  class="w-full px-3 py-1 focus:outline-none"
+                />
+                <button
+                  type="submit"
+                  class="px-6 py-2 text-white bg-gray-400 rounded-full  bs-button-bg"
+                >
+                  Search
+                </button>
+              </div>
+            </div>
+            <div class="flex items-center min-w-max">
+              <nuxt-link to="/">
+                <span
+                  class="flex items-center justify-center rounded-full  bs-icon-box hover:bg-gray-200"
+                >
+                  <img
+                    src="~/assets/img/heart.png"
+                    loading="lazy"
+                    alt="heart icon"
+                    srcset=""
+                  />
+                </span>
+              </nuxt-link>
+              <nuxt-link to="/" class="relative mx-4">
+                <span
+                  class="flex items-center justify-center rounded-full  bs-icon-box hover:bg-gray-200"
+                >
+                  <img
+                    src="~/assets/img/cart.png"
+                    loading="lazy"
+                    alt="cart icon"
+                    srcset=""
+                  />
+                </span>
+                <span
+                  class="absolute top-0 right-0 flex items-center justify-center w-4 h-4 text-xs text-white bg-orange-600 rounded-full "
+                  >0</span
+                >
+              </nuxt-link>
+              <nuxt-link to="/" class="flex items-center">
+                <span
+                  class="flex items-center justify-center rounded-full  bs-icon-box hover:bg-gray-200"
+                >
+                  <img
+                    src="~/assets/img/account.png"
+                    loading="lazy"
+                    alt="account icon"
+                    srcset=""
+                  />
+                </span>
+                <span>Account</span>
+              </nuxt-link>
+            </div>
+          </div>
         </div>
       </div>
 
       <div class="flex items-center">
         <div
           v-click-outside="onClickOutside"
-          class="relative w-96 header-slide-menu"
+          class="relative w-1/4 header-slide-menu"
         >
           <div
             @click="toogleMenu"
@@ -121,24 +128,28 @@
             </ul>
           </div>
         </div>
-        <div class="w-full">
-          <ul class="flex justify-center">
-            <li v-for="(menu, index) in mainMenu" :key="index">
-              <nuxt-link :to="menu.url" class="p-4 hover:text-green-300">{{
-                menu.title
-              }}</nuxt-link>
-            </li>
-          </ul>
+        <div class="flex items-center justify-between w-3/4 pl-20">
+          <div class="w-full">
+            <ul class="flex">
+              <li v-for="(menu, index) in mainMenu" :key="index">
+                <nuxt-link
+                  :to="menu.url"
+                  class="px-6 py-4 hover:text-green-300"
+                  >{{ menu.title }}</nuxt-link
+                >
+              </li>
+            </ul>
+          </div>
+          <nuxt-link to="/" class="flex items-center text-orange-600 min-w-max">
+            <img
+              src="~/assets/img/percentage.png"
+              loading="lazy"
+              alt="down"
+              srcset=""
+            />
+            <span class="ml-3">Speical Offers</span>
+          </nuxt-link>
         </div>
-        <nuxt-link to="/" class="flex items-center text-orange-600 min-w-max">
-          <img
-            src="~/assets/img/percentage.png"
-            loading="lazy"
-            alt="down"
-            srcset=""
-          />
-          <span class="ml-3">Speical Offers</span>
-        </nuxt-link>
       </div>
     </div>
   </div>
